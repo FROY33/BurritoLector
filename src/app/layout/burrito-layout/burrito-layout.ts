b4bf95e56ce83776122a882af4c2e69606200b04
froy33 <f90ymtz@gmail.com>
2026-06-22 13:59:00 -0600

    barra lateral

--- src/app/layout/burrito-layout/burrito-layout.ts
+++ src/app/layout/burrito-layout/burrito-layout.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2} from '@angular/core';
 import { Router, RouterOutlet, RouterLink } from "@angular/router";
 import { LoginService } from 'src/app/services/login-service';
 
@@ -11,10 +11,19 @@ import { LoginService } from 'src/app/services/login-service';
 })
 export class BurritoLayout {
 
-  constructor(private loginService: LoginService, private router: Router) {}
-
+  constructor(private loginService: LoginService, private router: Router, private renderer: Renderer2) {}
   cerrarSesion() {
     this.loginService.cerrarSesion();
     this.router.navigate(['/auth/login']);
-  } 
+  }
+
+  toggleSidebar() {
+    const body = document.body;
+    // La plantilla HexaDash usa la clase 'collapsed' en el body
+    if (body.classList.contains('collapsed')) {
+      this.renderer.removeClass(body, 'collapsed');
+    } else {
+      this.renderer.addClass(body, 'collapsed');
+    }
+  }
 }
